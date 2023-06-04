84a68ca2e9f206fdb9344e7c26e3f44b02847780
Nori Lacsina <toperrrrrrrr@gmail.com>
2023-06-04 13:22:58 +0800

    practice

---
+++ minuts.py
@@ -0,0 +1,22 @@
+#In one year, if there are 365 days, with 24 hours in a day, and 60 minutes in an hour, write a program to calculate the number of minutes in a year. Print the result on the screen. Note: Your result should be in the format of just a number, not a sentence.
+
+dayy = 365
+hour = 24
+minutes = 60 
+
+hoursindays = dayy * hour
+minutesinyear = hoursindays * minutes
+
+
+print(minutesinyear)
+print(15+5+(3*2)/4**2+(3-7)*7)
+
+total_computers = 30*20
+replacement_cycle = 5
+computers_per_year = total_computers/replacement_cycle
+
+
+print(computers_per_year)
+
+possibilities = 10 ** 8
+print(possibilities)
